5402c943dd90c74a725042b2724bd4868fab3295
mikseros <mmadynski@gmail.com>
2022-06-28 20:34:06 +0200

    CXreate Category & CategoryRepository

---
+++ src/main/java/com/mikseros/inventory/category/CategoryRepository.java
@@ -0,0 +1,7 @@
+package com.mikseros.inventory.category;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Integer> {
+
+}
