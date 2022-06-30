cee0518ad11e7bb91b14e289f8195c9158c081ec
mikseros <mmadynski@gmail.com>
2022-06-30 14:54:46 +0200

    End-point for save new Category in the DB.

--- src/main/java/com/mikseros/inventory/category/CategoryController.java
+++ src/main/java/com/mikseros/inventory/category/CategoryController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class CategoryController {
@@ -21,10 +22,17 @@ public class CategoryController {
 		return "categories";
 	}
 	
-	@GetMapping("categories/new")
+	@GetMapping("/categories/new")
 	public String showCategoryNewForm(Model model) {
 		model.addAttribute("category", new Category());
 		
 		return "category_form";
+	}
+	
+	@PostMapping("/categories/save")
+	public String saveCategory(Category category) {
+		repo.save(category);
+		
+		return "redirect:/categories";
 	}
 }
