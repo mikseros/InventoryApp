7af1c6d9c90cd2a5545768ab5ecc64a77d105a42
mikseros <mmadynski@gmail.com>
2022-07-01 23:03:17 +0200

    Delete Product -> Controller method

--- src/main/java/com/mikseros/inventory/product/ProductController.java
+++ src/main/java/com/mikseros/inventory/product/ProductController.java
@@ -56,4 +56,11 @@ public class ProductController {
 		
 		return "product_form";
 	}
+	
+	@GetMapping("products/delete/{id}")
+	public String deleteProduct(@PathVariable("id") Integer id, Model model) {
+		productRepo.deleteById(id);
+		
+		return "redirect:/products";
+	}
 }
