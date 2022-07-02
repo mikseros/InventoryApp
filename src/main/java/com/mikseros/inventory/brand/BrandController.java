e33c68bc9104bfe69223f6d05631f0693f93463c
mikseros <mmadynski@gmail.com>
2022-07-02 22:54:19 +0200

    BrandController -> Create Brand Mapping

---
+++ src/main/java/com/mikseros/inventory/brand/BrandController.java
@@ -0,0 +1,31 @@
+package com.mikseros.inventory.brand;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.mikseros.inventory.category.Category;
+import com.mikseros.inventory.category.CategoryRepository;
+
+@Controller
+public class BrandController {
+
+	@Autowired
+	private BrandRepository brandRepo;
+	
+	@Autowired
+	private CategoryRepository categoryRepo;
+	
+	@GetMapping("brands/new")
+	public String showCreateNewBrandForm (Model model) {
+		List<Category> listCategories = categoryRepo.findAll();
+		
+		model.addAttribute("listCategories", listCategories);
+		model.addAttribute("brand", new Brand());
+		
+		return "brand_form";
+	}
+}
