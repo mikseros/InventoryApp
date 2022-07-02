a7d09efdda92f22abceee85655d8c1032b01c994
mikseros <mmadynski@gmail.com>
2022-07-02 23:22:33 +0200

    Save new Brand to DB mapping

--- src/main/java/com/mikseros/inventory/brand/BrandController.java
+++ src/main/java/com/mikseros/inventory/brand/BrandController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.mikseros.inventory.category.Category;
 import com.mikseros.inventory.category.CategoryRepository;
@@ -27,5 +28,12 @@ public class BrandController {
 		model.addAttribute("brand", new Brand());
 		
 		return "brand_form";
+	}
+	
+	@PostMapping("/brands/save")
+	public String saveBrand(Brand brand) {
+		brandRepo.save(brand);
+		
+		return "redirect:/";
 	}
 }
