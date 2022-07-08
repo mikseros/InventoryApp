8ca98c4f7267442f86a297aaa17cbfaa82421163
mikseros <mmadynski@gmail.com>
2022-07-08 18:14:39 +0200

    Test for Roles creation

--- src/test/java/com/mikseros/inventory/UserRepositoryTests.java
+++ src/test/java/com/mikseros/inventory/UserRepositoryTests.java
@@ -5,8 +5,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.annotation.Rollback;
 
+import com.mikseros.inventory.user.Role;
 import com.mikseros.inventory.user.UserRepository;
 
 @DataJpaTest
@@ -17,8 +19,17 @@ public class UserRepositoryTests {
 	@Autowired
 	private UserRepository repo;
 	
+	@Autowired
+	private TestEntityManager entityManager;
+	
 	@Test
-	public void test() {
+	public void testCreateRoles() {
+		Role roleAdmin = new Role("Administrator");
+		Role roleEditor = new Role("Editor");
+		Role roleVisitor = new Role("Visitor");
 		
+		entityManager.persist(roleAdmin);
+		entityManager.persist(roleEditor);
+		entityManager.persist(roleVisitor);
 	}
 }
