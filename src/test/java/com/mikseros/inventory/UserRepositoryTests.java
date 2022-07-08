9498ac2943459de5c83beb6063e2c20e8fb29814
mikseros <mmadynski@gmail.com>
2022-07-08 14:10:26 +0200

    Test Class for UserRepository

---
+++ src/test/java/com/mikseros/inventory/UserRepositoryTests.java
@@ -0,0 +1,24 @@
+package com.mikseros.inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Rollback;
+
+import com.mikseros.inventory.user.UserRepository;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+@Rollback(false)
+public class UserRepositoryTests {
+
+	@Autowired
+	private UserRepository repo;
+	
+	@Test
+	public void test() {
+		
+	}
+}
