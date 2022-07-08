2157c255f342de99c8ce610f50f6e450b9e312b2
mikseros <mmadynski@gmail.com>
2022-07-08 14:09:03 +0200

    New Entity Class "Role"

---
+++ src/main/java/com/mikseros/inventory/user/Role.java
@@ -0,0 +1,31 @@
+package com.mikseros.inventory.user;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Role {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@Column(length = 25, nullable = false, unique = true)
+	private String name;
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+}
