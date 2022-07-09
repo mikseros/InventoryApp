a81e9774e84168d0a597bc72f0bf651e55d76a88
mikseros <mmadynski@gmail.com>
2022-07-09 23:14:26 +0200

    New constructor of Role entity. Only id

--- src/main/java/com/mikseros/inventory/user/Role.java
+++ src/main/java/com/mikseros/inventory/user/Role.java
@@ -1,5 +1,7 @@
 package com.mikseros.inventory.user;
 
+import java.util.Objects;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -17,6 +19,10 @@ public class Role {
 	private String name;
 	
 	public Role() {
+	}
+	
+	public Role(Integer id) {
+		this.id = id;
 	}
 	
 	public Role(Integer id, String name) {
@@ -40,4 +46,23 @@ public class Role {
 	public void setName(String name) {
 		this.name = name;
 	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Role other = (Role) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
 }
