c704cf9def361ac5a3a9fff6383bb4e9a2b55d00
mikseros <mmadynski@gmail.com>
2022-07-13 21:38:03 +0200

    Test for adding item from the DB to the cart.

--- src/test/java/com/mikseros/inventory/ShoppingCartTests.java
+++ src/test/java/com/mikseros/inventory/ShoppingCartTests.java
@@ -8,7 +8,10 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.annotation.Rollback;
 
+import com.mikseros.inventory.product.Product;
+import com.mikseros.inventory.shoppingcart.CartItem;
 import com.mikseros.inventory.shoppingcart.CartItemRepository;
+import com.mikseros.inventory.user.User;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = Replace.NONE)
@@ -22,7 +25,12 @@ public class ShoppingCartTests {
 	private TestEntityManager entityManager;
 	
 	@Test
-	public void test() {
+	public void testAddItemFromDatabase() {
+		Product product = entityManager.find(Product.class, 1);
+		User user = entityManager.find(User.class, 1);
 		
+		CartItem item = new CartItem(1, product, user);
+		
+		repo.save(item);
 	}
 }
