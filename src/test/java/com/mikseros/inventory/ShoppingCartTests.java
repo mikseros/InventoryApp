81e0df345455c30171e3039b963d2e206c1696fd
mikseros <mmadynski@gmail.com>
2022-07-14 21:15:28 +0200

    Test for adding multiple items.

--- src/test/java/com/mikseros/inventory/ShoppingCartTests.java
+++ src/test/java/com/mikseros/inventory/ShoppingCartTests.java
@@ -1,5 +1,7 @@
 package com.mikseros.inventory;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
@@ -42,5 +44,20 @@ public class ShoppingCartTests {
 		CartItem item = new CartItem(2, product, user);
 		
 		repo.save(item);
+	}
+	
+	@Test
+	public void testAddMultipleItems() {
+		User user = new User(1);
+		Product product1 = new Product(3);
+		Product product2 = new Product(2);
+		Product product3 = new Product(1);
+		
+		CartItem item1 = new CartItem(1, product1, user);
+		CartItem item2 = new CartItem(5, product2, user);
+		CartItem item3 = new CartItem(4, product3, user);
+		
+		repo.saveAll(List.of(item1, item2, item3));
+		
 	}
 }
